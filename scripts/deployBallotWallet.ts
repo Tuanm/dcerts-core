4e10461e6d4ff21b0da6ed536aeb78d1eb0190d9
Do Tuan <tuanm.dev@gmail.com>
2022-07-25 13:41:59 +0700

    nothing

--- scripts/deployBallotWallet.ts
+++ scripts/deployBallotWallet.ts
@@ -1,7 +1,7 @@
 import { ethers } from 'hardhat';
 
 async function main() {
-    const execution = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
+    const execution = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
     const voters = [
         '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
         '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
